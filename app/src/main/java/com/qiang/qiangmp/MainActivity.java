2cc42efe7537ec3be64cf56c2f9259c6370b04fd
zouxiaoqiang <1182523747@qq.com>
2019-01-09 00:30:15 +0800

    First commit.

---
+++ app/src/main/java/com/qiang/qiangmp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.qiang.qiangmp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author xiaoq
+ * @date 19-1-8
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
